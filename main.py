7f1118e4fb647270d7031c801138fc84e7398448
Dmytro <pytakdima@gmail.com>
2025-07-18 13:07:04 +0300

    html template added, also 3.py renamed to main.py

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ def logout():
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("3:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
